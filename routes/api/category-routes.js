aa28dc8a2c0dfd4f31cdb51a9cd02e8cf84214d5
bebo <ibrahimimran96@gmail.com>
2022-10-23 02:43:24 -0400

    Added product CRUD functionality, started category CRUD functionality

---
+++ routes/api/category-routes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    order: [['created_at', 'DESC']],
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name', 'price', 'stock', 'category_id'],
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    where: req.params.id,
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name', 'price', 'stock', 'category_id'],
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then((dbCategoryData) => res.status(200).json(dbCategoryData))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
